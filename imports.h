35dcf76fa1dd65e4f9cc44acb43a8cd5ab6ec356
Benas Žitkevičius <benasi89@gmail.com>
2020-02-21 22:58:13 +0200

    Add files via upload

---
+++ imports.h
@@ -0,0 +1,33 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <random>
+#include <ctime>
+#include <algorithm>
+//#include "functionDec.h"
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::endl;
+using std::setw;
+
+struct stud {
+	string name, lname;
+	int n = 0;
+	float exam;
+	std::vector<int> nd;
+};
+
+void CreateInput(int, int);
+int InLen(string);
+void Input(int, stud*, string);
+float Final(float, float);
+float Average(stud);
+float Median(stud);
+void Output(int, stud*);
+void Output2file(int, stud*);
+bool cmpr(stud, stud);
+string whichFile();

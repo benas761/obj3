03f35100a9f8639fd0a158df52f6d7d2265e91c3
Benas Žitkevičius <benasi89@gmail.com>
2020-02-23 14:57:14 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
 #include "imports.h"
-//#include "functionDec.h"
 
 int main() {
 	string input = whichFile(); // Tikrina ar yra kursiokai.txt failas
 	const int n = InLen(input);
-	//try {
+	try {
 		stud *x = new stud[n];
 		Input(n, x, input);
-		//std::sort(x, x+n, cmpr);
+		std::sort(x, x+n, cmpr);
 		Output(n, x);
 		Output2file(n, x);
 		delete[] x;
-	//} catch(std::exception& e) {
-	//	cout << "Papildykite kursiokai.txt faila arba ji istrinkite" << endl;
+	} catch(std::exception& e) {
+		cout << "Papildykite kursiokai.txt faila arba ji istrinkite" << endl;
 		// if(n == -1)
-	//}
+	}
 	return 0;
 }

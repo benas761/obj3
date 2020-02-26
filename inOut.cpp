43ae85adbf9760b7aaa8b9ce395d9687f8c64dfd
Benas Žitkevičius <benasi89@gmail.com>
2020-02-26 10:14:48 +0200

    Add files via upload
    Neleidžia įkelti didesnių negu 25mb failų, tad negaliu įkelti likusių sugeneruotų .txt failų.

--- inOut.cpp
+++ inOut.cpp
@@ -1,31 +1,20 @@
 #include "imports.h"
 
-void CreateInput(int n, int m) {
-	int num = 32;
+void CreateInput(int n, int m) { // pakeista i sablona
 	std::random_device rd;
 	std::mt19937 mt0(rd());
-	std::mt19937 mt1(rd());
-	std::uniform_int_distribution<int> names(0, num-1);
 	std::uniform_int_distribution<int> grades(1, 10);
-	string name[] = {
-		"Lisa", "Tom", "Steve", "Karen", "Jimmy", "Tim", "Frank", "Charlie", "Charlotte", "Abus", "Gus",
-		"Vincent", "Douglas", "Kim", "Jim", "Henry", "Lenny", "Homer", "Kate", "Stacy", "Mary", "Jenny",
-		"Susy", "Quin", "George", "Gin", "Asira", "Amy", "Abigail", "Thomas"
-	};
-	string lname[] =  {
-		"Jobs", "Williams", "Smith", "Johnson", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore",
-		"Taylor", "Anderson", "Thoas", "Jackson", "White", "Black", "Pink", "Harris", "Martin", "Thompson",
-		"Garcia", "Martinez", "Robinson", "Clark", "Lewis", "Lee", "Walker", "Hall", "Allen", "Young",
-		"King", "Kernel"
-	};
-	std::ofstream fr("Generated.txt");
+	std::ofstream fr("1000.txt");
 	fr << setw(33) << std::left << "Vardas" << setw(30) << std::left << "Pavarde";
 	for(int j = 0; j < m; j++) fr << setw(3) << std::right << j+1 << "nd";
 	fr << setw(5) << "egz." << endl;
-	for(int i=0; i<n; i++) {
-		fr << setw(33) << std::left <<  name[names(mt0)];
-		fr << setw(33) << std::left << lname[names(mt0)];
-		for(int j = 0; j < m+1; j++) fr << setw(5) << grades(mt1);
+	for(int i=1; i<=n; i++) {
+		//printf("Vardas%33d Pavarde%33d", i, i);
+		fr << "Vardas" << setw(27) << std::left << i;
+		fr << "Pavarde" << setw(26) << std::left << i; 
+		//fr << setw(33) << std::left <<  name[names(mt0)];
+		//fr << setw(33) << std::left << lname[names(mt0)];
+		for(int j = 0; j < m+1; j++) fr << setw(5) << grades(mt0);
 		fr << endl;
 	}
 	fr.close();
@@ -81,16 +70,23 @@ void Output(int n, stud x[]) {
 }
 
 void Output2file(int n, stud x[]) {
-	std::ofstream fr("results.txt");
-	fr << setw(18) << std::left << "Vardas" << setw(18) << std::left << "Pavarde" << "Galutinis (Vid.) / Galutinis (Med.)\n";
-    fr <<"------------------------------------------------------------\n";
+	std::ofstream bad("nepatenkinami.txt");
+	std::ofstream good("patenkinami.txt");
+	bad << setw(18) << std::left << "Vardas" << setw(18) << std::left << "Pavarde" << "Galutinis (Vid.) / Galutinis (Med.)\n" <<"------------------------------------------------------------\n";
+    good << setw(18) << std::left << "Vardas" << setw(18) << std::left << "Pavarde" << "Galutinis (Vid.) / Galutinis (Med.)\n" <<"------------------------------------------------------------\n";
     for(int i=0; i<n; i++) {
-	    fr <<setw(18)<<std::left<<x[i].name<<setw(18)<<std::left<<x[i].lname;
-		if(n == 0)
-	    	 fr <<std::setw(19)<<std::left<<Final(0, x[i].exam)<<endl;
-		else fr <<std::setw(19)<<std::left<<Average(x[i])<<setw(16)<<std::left<<Median(x[i])<<endl;
+    	float avg = Average(x[i]);
+	    if(avg >= 5.0) {
+	    	good <<setw(18)<<std::left<<x[i].name<<setw(18)<<std::left<<x[i].lname;
+			good <<std::setw(19)<<std::left<<avg<<setw(16)<<std::left<<Median(x[i])<<endl;
+		}
+		else {
+	    	bad <<setw(18)<<std::left<<x[i].name<<setw(18)<<std::left<<x[i].lname;
+			bad <<std::setw(19)<<std::left<<avg<<setw(16)<<std::left<<Median(x[i])<<endl;
+		}
 	}
-	fr.close();
+	good.close();
+	bad.close();
 }
 
 bool cmpr(stud a, stud b) {
@@ -107,7 +103,7 @@ bool youAreHere() {
 string whichFile() {
 	if (youAreHere()) return("kursiokai.txt");
 	else {
-		CreateInput(25, 5); // Kiek mokiniu, kiek namu darbu
-		return("Generated.txt");
+		CreateInput(1000, 5); // Kiek mokiniu, kiek namu darbu
+		return("1000.txt");
 	}
 }
